d82024a37aa49f1ae9d52b504ccebe5d37ae4489
ChirujanoCodding <trdfabris@gmail.com>
2022-09-13 14:54:47 -0300

    util stuff

---
+++ src/utils/event_starter.py
@@ -0,0 +1,30 @@
+from ..models.Event import Event
+from ..models.EventManager import EventManager
+from ..events.new_order import NewOrder
+from ..events.new_customer import NewCustomer
+
+def setup_listener(listener: EventManager, pre_events: list[Event] = [NewOrder('NewTicket'),NewCustomer('NewCustomer')]) -> EventManager:
+    """
+    setup the event manager
+    `listener`: EventManager
+    `pre_events`: list[Event] | None
+    ### @example
+    ```py
+    # Example 1
+    EM = EventManager()
+    EM = setup_listener(EM)
+    #Example 2
+    EM = setup_listener(EventManager())
+
+    #Example 3
+    from events import Event1, Event2
+    EM = setup_listener(EventManager(), [Event1, Event2])
+    ```
+    """
+    try:
+        for pre in pre_events:
+            listener.add_event(pre)
+        return listener
+    except:
+        return Exception()
+    
---
+++ src/utils/functions.py
@@ -0,0 +1,5 @@
+import uuid
+
+
+def generate_random_id():
+    return uuid.uuid1()

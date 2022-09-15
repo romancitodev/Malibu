6812eb2cccc0879bcf1b5fa52e2e649ee839dff2
ChirujanoCodding <trdfabris@gmail.com>
2022-09-14 22:02:52 -0300

    Revert "typed events"
    This reverts commit 4747447f7c0e13e8bad64792f4c726f015233592.

--- src/events/new_customer.py
+++ src/events/new_customer.py
@@ -1,3 +1,4 @@
+
 from src.models.Event import Event
 from src.models.Customer import CustomerBase
 
@@ -9,8 +10,8 @@ class NewCustomer(Event):
     def __init__(self, name_event: str) -> None:
         super().__init__(name_event)
     
-    def run(self, customer: CustomerBase) -> None: # type: ignore
+    def run(self, customer: CustomerBase) -> None:
         self.logging.info(' New Customer '.center(50, '-'))
-        self.logging.info(f'[Customer]: {customer.customer_info.get("name", "").capitalize()}')
+        self.logging.info(f'[Customer]: {customer.customer_info.get("name").capitalize()}')
         self.logging.info(f'[Customer]: {customer.customer_info.get("type")}') 
         self.logging.info(f'[Customer]: {customer.customer_info.get("id")}') 
--- src/events/new_employee.py
+++ src/events/new_employee.py
@@ -1,6 +1,5 @@
 
 
-# from typing import Any
 from src.models.Event import Event
 from src.models.Employee import EmployeeBase
 
@@ -12,8 +11,8 @@ class NewEmployee(Event):
     def __init__(self, name_event: str) -> None:
         super().__init__(name_event)
     
-    def run(self, employee: EmployeeBase) -> None: #type: ignore
+    def run(self, employee: EmployeeBase) -> None:
         self.logging.info(' Employee logged  '.center(50, '-'))
-        self.logging.info(f'[Employee]: {employee.employee_info.get("username","").capitalize()}')
+        self.logging.info(f'[Employee]: {employee.employee_info.get("username").capitalize()}')
         self.logging.info(f'[Employee]: {employee.employee_info.get("type")}') 
-        self.logging.info(f'[Employee]: {employee.employee_info.get("id")}')
+        self.logging.info(f'[Employee]: {employee.employee_info.get("id")}') 

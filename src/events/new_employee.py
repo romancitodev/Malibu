4747447f7c0e13e8bad64792f4c726f015233592
ChirujanoCodding <trdfabris@gmail.com>
2022-09-14 22:02:21 -0300

    typed events

--- src/events/new_employee.py
+++ src/events/new_employee.py
@@ -1,5 +1,6 @@
 
 
+# from typing import Any
 from src.models.Event import Event
 from src.models.Employee import EmployeeBase
 
@@ -11,8 +12,8 @@ class NewEmployee(Event):
     def __init__(self, name_event: str) -> None:
         super().__init__(name_event)
     
-    def run(self, employee: EmployeeBase) -> None:
+    def run(self, employee: EmployeeBase) -> None: #type: ignore
         self.logging.info(' Employee logged  '.center(50, '-'))
-        self.logging.info(f'[Employee]: {employee.employee_info.get("username").capitalize()}')
+        self.logging.info(f'[Employee]: {employee.employee_info.get("username","").capitalize()}')
         self.logging.info(f'[Employee]: {employee.employee_info.get("type")}') 
-        self.logging.info(f'[Employee]: {employee.employee_info.get("id")}') 
+        self.logging.info(f'[Employee]: {employee.employee_info.get("id")}')

8b8676d7a7166eed845fd5fc32c54ead5783c94e
ChirujanoCodding <trdfabris@gmail.com>
2022-09-17 20:31:43 -0300

    Implemented class

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from src.models.Interface import App
-from types.Employee import EmployeeDict
+from src.types.Employee import EmployeeDict
 
 app = App()
 
 dummy_name = 'Dummy'
 dummy_password = "DummyPassword01!"
-credentials: EmployeeDict = {
-    'username': dummy_name,
-    'password': dummy_password
-}
+credentials = EmployeeDict(dummy_name, dummy_password)
 app.start(credentials,1)
 
+
+
+        

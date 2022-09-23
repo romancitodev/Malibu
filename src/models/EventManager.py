5669e50f9f0cb1bbce57a209dc21d5fb4c6c897a
ChirujanoCodding <trdfabris@gmail.com>
2022-09-23 16:20:56 -0300

    added Importer functionality

--- src/models/EventManager.py
+++ src/models/EventManager.py
@@ -1,35 +1,18 @@
-import os
-import sys
 from typing import Any
 from typing_extensions import Self
 from .Event import Event
-from pathlib import Path
+from .Importer import Importer
 
 class EventManager:
     
     def __init__(self) -> None:
         self.events: dict[str,Event] = dict()
 
-    def setup(self, events_path:Path | str | None = None) -> Self:
-        """setup the event manager with pre-charged events"""
-        if events_path:
-            events_files = [f for f in os.listdir(events_path) if os.path.isfile(os.path.join(events_path, f))]
-            for c in events_files:
-                file = open(f'{events_path}/{c}', 'r')
-                c = c.split('.')[0]
-                text = file.read()
-                if len(text) > 0:
-                    class_declaration = text.find('class') + 6
-                    class_name = text.find('(', class_declaration)
-                    class_name = text[class_declaration:class_name]
-                    sys.path.append(r'{}'.format(events_path))
-                    a = __import__(r'{}'.format(c))
-                    if not getattr(a, class_name).__base__ == Event:
-                        continue
-                    self.events[class_name] = getattr(a, class_name)(class_name)
-                    print(f'[!] Event {class_name:10s} Charged succesfully.')
-        else:
-            raise Exception(f'{events_path} is not a valid Path.')
+    def setup(self, events_path:str) -> Self:
+        try:
+            self.events = Importer().import_from(events_path, superclass= Event, verboose=True)
+        except Exception as err:
+            print(err)
         return self
 
     def add_event(self, event_class: Event) -> None:
